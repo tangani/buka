bd6ebb4b4c2cacb19b46f8f9ac7b2a6f9ebea9ce
tangani <tanganimoyo@gmail.com>
2023-06-28 22:51:46 +0200

    Adding vertical scrolling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crossterm::terminal::ClearType;
 use crossterm::{event::*, cursor};
 use crossterm::{event, execute, queue, terminal};
+use std::path::Path;
+use std::{cmp, env, fs, io};
 use std::io::{ErrorKind, Result, stdout, Write};
 use std::time::Duration;
 
@@ -113,20 +115,27 @@ impl Output {
         let screen_rows = self.win_size.1;
         let screen_columns = self.win_size.0;
         for i in 0..screen_rows {
-            if i == screen_rows / 3 {
-                let mut welcome = format!("Buka editor - VERSION: {}", VERSION);
-                if welcome.len() > screen_columns {
-                    welcome.truncate(screen_columns);
-                }
-                let mut padding = (screen_columns - welcome.len()) / 2;
-                if padding != 0 {
+            let file_row = i + self.cursor_controller.row_offset;
+            if file_row >= self.editor_rows.number_of_rows() {
+                if self.editor_rows.number_of_rows == 0 && i == screen_rows / 3 {
+                    let mut welcome = format!("Buka editor - VERSION: {}", VERSION);
+                    if welcome.len() > screen_columns {
+                        welcome.truncate(screen_columns);
+                    }
+                    let mut padding = (screen_columns - welcome.len()) / 2;
+                    if padding != 0 {
+                        self.editor_contents.push('~');
+                        padding -= 1;
+                    }
+                    (0..padding).for_each(|_| self.editor_contents.push(' '));
+                    self.editor_contents.push_str(&welcome);
+                } else {
                     self.editor_contents.push('~');
-                    padding -= 1;
                 }
-                (0..padding).for_each(|_| self.editor_contents.push(' '));
-                self.editor_contents.push_str(&welcome);
             } else {
-                self.editor_contents.push('~');
+                let len = cmp::min(self.editor_rows.get_row(file_row).len(), screen_columns);
+                self.editor_contents
+                    .push_str(&self.editor_rows.get_row(file_row)[..len])
             }
             queue!(
                 self.editor_contents,
@@ -140,6 +149,7 @@ impl Output {
     }
 
     fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        self.cursor_controller.scroll();
         queue!(
             self.editor_contents,
             cursor::Hide,
@@ -147,7 +157,7 @@ impl Output {
         )?;
         self.draw_rows();
         let cursor_x = self.cursor_controller.cursor_x;
-        let cursor_y = self.cursor_controller.cursor_y;
+        let cursor_y = self.cursor_controller.cursor_y - self.cursor_controller.row_offset;
         queue!(
             self.editor_contents,
             cursor::MoveTo(cursor_x as u16, cursor_y as u16),
@@ -157,7 +167,7 @@ impl Output {
     }
 
     fn move_cursor(&mut self, direction: KeyCode) {
-        self.cursor_controller.move_cursor(direction);
+        self.cursor_controller.move_cursor(direction, self.editor_rows.number_of_rows());
     }
 }
 
@@ -207,6 +217,7 @@ struct CursorController {
     cursor_y: usize,
     screen_columns: usize,
     screen_rows: usize,
+    row_offset: usize,
 }
 
 
@@ -217,10 +228,11 @@ impl  CursorController {
             cursor_y: 0,
             screen_columns: win_size.0,
             screen_rows: win_size.1,
+            row_offset: 0,
         }
     }
 
-    fn move_cursor(&mut self, direction: KeyCode) {
+    fn move_cursor(&mut self, direction: KeyCode, number_of_rows: usize) {
         match direction {
             KeyCode::Up => self.cursor_y = self.cursor_y.saturating_sub(1),
             KeyCode::Left => {
@@ -229,7 +241,7 @@ impl  CursorController {
                 }
             },
             KeyCode::Down => {
-                if self.cursor_y != self.screen_rows - 1 {
+                if self.cursor_y != number_of_rows {
                     self.cursor_y += 1;
                 }
             },
@@ -243,6 +255,13 @@ impl  CursorController {
             _ => unimplemented!(),
         }
     }
+
+    fn scroll(&mut self) {
+        self.row_offset = cmp::min(self.row_offset, self.cursor_y);
+        if self.cursor_y >= self.row_offset + self.screen_rows {
+            self.row_offset = self.cursor_y - self.screen_rows + 1;
+        }
+    }
 }
 
 
@@ -254,17 +273,30 @@ struct EditorRows {
 
 impl EditorRows {
     fn new() -> Self {
+        let mut arg: env::Args = env::args();
+
+        match arg.nth(1) {
+            None => Self {
+                row_contents: Vec::new(),
+            },
+            Some(file) => Self::from_file(file.as_ref()),
+            _ => unimplemented!()
+        }
+    }
+
+    fn from_file(file: &Path) -> Self {
+        let file_contents = fs::read_to_string(file).expect("Unable to read file");
         Self {
-            row_contents: vec!["Welcome to buka".into()],
+            row_contents: file_contents.lines().map(|it| it.into()).collect(),
         }
     }
 
     fn number_of_rows(&self) -> usize {
-        1
+        self.row_contents.len()
     }
 
-    fn get_row(&self) -> &str {
-        &self.row_contents[0]
+    fn get_row(&self, at: usize) -> &str {
+        &self.row_contents[at]
     }
 }
 
